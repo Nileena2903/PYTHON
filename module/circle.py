10ea46916e9f7d33410c70af988e5d44bd074b95
Nileena2903 <116252133+Nileena2903@users.noreply.github.com>
2023-01-20 15:07:45 +0530

    Delete module directory

--- module/circle.py
+++
@@ -1,6 +0,0 @@
-#area of circle 
-
-def circarea(r):
-    print("Area=",3.14*r*r)
-def circperi(r):
-    print("Perimeter=",2*3.14*r)
--- module/rectangle.py
+++
@@ -1,7 +0,0 @@
-#area and perimeter of rectangle
-
-def rectarea(l,b):
-    print("Area=",l*b)
-    
-def rectperi(l,b):
-    print("Perimeter=",2*(l+b))
