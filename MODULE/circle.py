b5f46499e88b302bfd75b4d916832a6564edeff3
Nileena2903 <116252133+Nileena2903@users.noreply.github.com>
2023-01-20 15:13:40 +0530

    Add files via upload

---
+++ MODULE/circle.py
@@ -0,0 +1,6 @@
+#area of circle 
+
+def circarea(r):
+    print("Area=",3.14*r*r)
+def circperi(r):
+    print("Perimeter=",2*3.14*r)
---
+++ PACKAGE/Graphics/Graphics3D/cuboid.py
@@ -0,0 +1,6 @@
+#area of cuboid
+
+def cubarea(l,b,h):
+    print("Area =",2*(l*b)+(l*h)+(b*h))
+def cubperi(l,b,h):
+    print("perimeter=",4*(l+b+h))
