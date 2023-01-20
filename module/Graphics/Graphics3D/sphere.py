ec69dca9ee1055ec31fb1273b8d2b4ecc3f0ed28
Nileena2903 <116252133+Nileena2903@users.noreply.github.com>
2023-01-20 14:46:01 +0530

    Add files via upload

---
+++ module/Graphics/Graphics3D/sphere.py
@@ -0,0 +1,6 @@
+#area of sphere
+
+def sparea(r):
+    print("Area=",2*3.14*r)
+def spperi(r):
+    print("Perimeter =",4*3.14*r*r)   
---
+++ module/rectangle.py
@@ -0,0 +1,7 @@
+#area and perimeter of rectangle
+
+def rectarea(l,b):
+    print("Area=",l*b)
+    
+def rectperi(l,b):
+    print("Perimeter=",2*(l+b))
